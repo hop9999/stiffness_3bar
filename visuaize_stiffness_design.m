00efa697a9ddfd83b529a2f45d129aeb7cb786c5
SergeiSa <sergey89mtkgtu@mail.ru>
2020-01-14 16:46:06 +0300

    Graphs prettyfication

--- visuaize_stiffness_design.m
+++ visuaize_stiffness_design.m
@@ -20,7 +20,8 @@ ax.GridAlpha = 0.6;
 ax.LineWidth = 0.5;
 ax.MinorGridLineStyle = '-';
 ax.MinorGridAlpha = 0.2;
-ax.FontName = 'Tibetan Machine Uni';
+% ax.FontName = 'Tibetan Machine Uni';
+ax.FontName = 'Times New Roman';
 ax.FontSize = 16;
 
 xlabel_handle = xlabel('$$n$$');
@@ -34,16 +35,18 @@ legend_handle.Interpreter = 'latex';
 fig = figure('Color', 'w');
 fig.Name = "err";
  hold on;
-grid on
-plot( f_err_arr, 'LineWidth', 2, 'LineStyle', '-', 'Color', [0 1 0]);
+grid on; grid minor;
+plot( f_err_arr, 'LineWidth', 2, 'LineStyle', '-', 'Color', [0 0.5 0]);
+plot( f_err_arr, 'o', 'MarkerEdgeColor', [0 0.5 0]);
 
 ax = gca;
-ax.FontName = 'Tibetan Machine Uni';
+% ax.FontName = 'Tibetan Machine Uni';
+ax.FontName = 'Times New Roman';
 ax.FontSize = 16;
 
 xlabel_handle = xlabel('$$n$$');
 xlabel_handle.Interpreter = 'latex';
-ylabel_handle = ylabel('$$||error||$$');
+ylabel_handle = ylabel('$$\varepsilon$$, N');
 ylabel_handle.Interpreter = 'latex';
-legend_handle = legend('$$force\ error$$');
-legend_handle.Interpreter = 'latex';
+% legend_handle = legend('$$force\ error$$');
+% legend_handle.Interpreter = 'latex';
